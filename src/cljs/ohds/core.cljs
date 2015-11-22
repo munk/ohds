fd3ddbc85154cb7d1587f2ce3eec904eecadf60c
John <john.downs@acm.org>
2015-11-22 10:50:01 -0500

    Removing hamburger from core

--- src/cljs/ohds/core.cljs
+++ src/cljs/ohds/core.cljs
@@ -37,17 +37,6 @@
            :class "form-control"
            :on-change #(reset! value (-> % .-target .-value))}])
 
-(defn hamburger []
-  [:button {:type "button"
-            :class "navbar-toggle collapsed"
-            :data-toggle "collapse"
-            :data-target "#navbar"
-            :aria-expanded "false" :aria-controls "navbar"}
-   [:span {:class "sr-only"} "Toggle Nav"]
-   [:span {:class "icon-bar"}]
-   [:span {:class "icon-bar"}]
-   [:span {:class "icon-bar"}]])
-
 (defn nav-bar []
   [:div {:id "navbar" :class "navbar-collapse collapse" :aria-expanded "false"}
    [:ul {:class "nav navbar-nav"}

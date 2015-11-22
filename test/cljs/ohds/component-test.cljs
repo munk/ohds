770779f0c61f50a9b6e54661f83df71445fe5930
John <john.downs@acm.org>
2015-11-22 10:42:43 -0500

    fixing hamburger test ignoring the rest of generated js files

---
+++ test/cljs/ohds/component-test.cljs
@@ -0,0 +1,13 @@
+(ns ohds.component-test
+  (:require-macros [cljs.test :refer (is deftest testing)])
+  (:require [cljs.test]
+            [reagent.core :as reagent :refer [atom]]
+            [ohds.components :as c]))
+
+(deftest example-passing-test
+  (let [h (c/hamburger)]
+    (is (= (get h 0) :button))
+    (is (= (first (get h 2)) :span))
+    (is (= (get h 3) (get h 4) (get h 5)))))
+
+

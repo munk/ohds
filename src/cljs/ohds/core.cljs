7f2b3820ed75b5e8a510fd81f18fdfbb98fd28eb
John <john.downs@acm.org>
2015-11-22 11:24:23 -0500

    removing login page from core

--- src/cljs/ohds/core.cljs
+++ src/cljs/ohds/core.cljs
@@ -24,23 +24,6 @@
   (swap! app-state assoc :page :location)
   (println "Logging in" username password @app-state))
 
-(defn login-page []
-  (let [username (atom "Username")
-        password (atom "Password")]
-    (swap! app-state assoc :fieldworker-id nil :location-id nil :individual-id nil)
-    (fn []
-      [:div {:class "form-signin"}
-       [:h2 {:class "form-signin-heading"} "Please log in"]
-       [:div
-        [:label {:for "username"} "Username"] [c/atom-input username "username"]]
-       [:div
-        [:label {:for "password"} "Password"] [c/atom-input-password password "password"]]
-       [:div {:class "checkbox"}]
-       [:div
-        [:button {:class "btn btn-lg btn-primary btn-block" :type "submit"
-                  :on-click (fn [] (do-login username password))}
-         "Login"]]])))
-
 (defn location-page []
   [:div "locations"])
 

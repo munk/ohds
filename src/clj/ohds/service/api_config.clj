2a8f992cdd74d38912a318f7a6505c4a19070516
John Downs <john.downs@acm.org>
2015-11-23 23:11:59 -0500

    removing old service namespace

--- src/clj/ohds/service/api_config.clj
+++
@@ -1,8 +0,0 @@
-(ns ohds.service.api-config
-  (:require [clojure.edn :as edn])
-  (:gen-class))
-
-(def config (edn/read-string (slurp "/Users/jdowns/ohds/application.edn")))
-(def apihost (:apihost config))
-(def apiuser (:apiuser config))
-(def apipass (:apipass config))
--- src/clj/ohds/service/time.clj
+++
@@ -1,5 +0,0 @@
-(ns ohds.service.time
-  (:import (java.time LocalDateTime)))
-
-(defn now []
-  (str (LocalDateTime/now) "Z"))

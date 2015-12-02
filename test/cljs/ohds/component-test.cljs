a8bc1b3411a3ea3d7f41d8508a1ee75af7de585c
John Downs <john.downs@acm.org>
2015-12-02 08:27:10 -0500

    test for atom changes in components

--- test/cljs/ohds/component-test.cljs
+++ test/cljs/ohds/component-test.cljs
@@ -4,6 +4,18 @@
             [reagent.core :as reagent :refer [atom]]
             [ohds.components :as c]))
 
+(deftest atom-change-tests
+  (let [state (atom {:hello "world"})]
+    (testing "a-swap only swaps out single key"
+      (c/a-swap state :hello "foo")
+      (is (= (:hello @state) "foo")))
+    (testing "on-change resets atom"
+      (let [value :reset-it
+            target (js-obj "value" value)
+            elem (js-obj "target" target)]
+        (c/on-change elem state)))))
+
+
 (deftest hamburger-menu-test
   (let [h (c/hamburger)]
     (is (= (get h 0) :button))

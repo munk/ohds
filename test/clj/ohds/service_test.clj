9d8c74bbe98b1adff83bbcd0c68414fdf2e0d109
John Downs <john.downs@acm.org>
2015-12-08 08:23:42 -0500

    adding test for http get

--- test/clj/ohds/service_test.clj
+++ test/clj/ohds/service_test.clj
@@ -1,11 +1,18 @@
 (ns ohds.service-test
   (:require
    [clojure.test :refer :all]
-   [ohds.service :refer :all])
-  (:import (java.time LocalDateTime)))
+   [ohds.service :refer :all]
+   [org.httpkit.client :as http])
+)
 
 (deftest json->cljs-test
   (is (= {:foo "bar"} (json->cljs "{\"foo\": \"bar\"}"))))
 
 (deftest http-methods-test
-  (testing "Testing get"))
+  (testing "Testing get"
+    (with-redefs [http/get
+                  (fn [url req]
+                    (is (= url "http://localhost:8080/foo"))
+                    (is (= req {:basic-auth ["user" "password"]}))
+                    (atom {:body "{\"uuid\": \"abc-123\"}"}))]
+      (is (= (get "/foo") {:uuid "abc-123"})))))

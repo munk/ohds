3cfb4f41d776b2ecb21ddce4972c71be0935cf89
John Downs <john.downs@acm.org>
2015-12-15 21:07:14 -0500

    adding route end to end tests

---
+++ test/clj/ohds/server_test.clj
@@ -0,0 +1,29 @@
+(ns ohds.server-test
+  (:require
+   [ohds.server :refer :all])
+  (:use expectations))
+
+(defn request [resource app method & params]
+  (app {:request-method method :uri resource :form-params (first params)}))
+
+(expect 401 (:status (request "/api/v1/login" routes :post
+                              {"username" "fieldworker"
+                               "password" "badpassword"})))
+
+(expect 200 (:status (request "/api/v1/login" routes :post
+                              {"username" "fieldworker"
+                               "password" "password"})))
+
+(expect AssertionError (:status (request "/api/v1/login" routes :post
+                              {"username" nil
+                               "password" "badpassword"})))
+
+(expect 200 (:status (request "/api/v1/locationHierarchy" routes :get)))
+
+(expect 200 (:status (request "/api/v1/locations/1" routes :get)))
+
+(expect 200 (:status (request "/api/v1/locations/" routes :get)))
+
+(expect 201 (:status (request "/api/v1/locations" routes :post {})))
+
+(expect 201 (:status (request "/api/v1/individuals" routes :post {})))

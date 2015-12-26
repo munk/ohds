b333c2a0cbad811ddbe3c1957acbf37cfe699abe
John Downs <john.downs@acm.org>
2015-12-25 23:15:55 -0500

    removing old login code

--- src/cljs/ohds/views.cljs
+++ src/cljs/ohds/views.cljs
@@ -6,25 +6,6 @@
    [schema.core :as s :include-macros true]
    [petrol.core :refer [send! send-value!]]))
 
-
-(defn login [ui-channel app]
-  [:div {:class "form-signin"}
-   [:h2 {:class "form-signin-heading"}
-    "Please log in"]
-   [:div
-    [:input {:id "username"
-             :type :text
-             :placeholder "Username"
-             :on-change (send-value! ui-channel m/->ChangeUsername)}]]
-   [:div
-    [:input {:id "password"
-             :type :password
-             :placeholder "Password"
-             :on-change (send-value! ui-channel m/->ChangePassword)}]]
-   [:div
-    [:button {:on-click (send! ui-channel (m/->FieldworkerLogin))}
-     "Login"]]])
-
 (defn ohds-option [field]
   (s/validate ml/LocationOption field)
   (let [{uuid :uuid name :name} field]

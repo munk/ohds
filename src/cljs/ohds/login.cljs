47079c9178988d72cde46b5efbd02df5c7c72358
John Downs <john.downs@acm.org>
2016-02-16 22:10:47 -0500

    copy login to compressed module

--- src/cljs/ohds/login.cljs
+++ src/cljs/ohds/login.cljs
@@ -1,6 +1,9 @@
 (ns ohds.login
   (:require [petrol.core :refer [wrap Message EventSource]]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            [ohds.login.messages :as m]
+            [ohds.components :as c]
+            [ohds.model :refer [valid-login?]]))
 
 (defrecord LoginResult [body])
 
@@ -18,3 +21,14 @@
        map->LoginResult
        (http/post "/api/v1/login"
                   {:form-params (:login app)}))}))
+
+
+(defn login [ch state]
+  [:span
+   [:legend "Login"]
+   (c/text-input ch m/->ChangeUsername "username" "Username" state)
+   (c/password-input ch m/->ChangePassword "password" "Password")
+   (c/checkbox "administrator" "Administrator" ch (m/->ToggleAdmin))
+
+   (when (valid-login? state)
+     (c/submit ch m/->FieldworkerLogin "Login"))])
